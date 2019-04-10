94102d43b9abe4d05df6c8b8273c0e2b9e42d558
Evan Wheeler <effyn@gmx.com>
2019-04-10 16:12:50 -0400

    Hola Mundo

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+require_once('vendor/autoload.php');
+
+$f3 = Base::instance();
+
+$f3 -> route("GET /", function() {
+    echo '¡Hola!';
+});
+
+$f3 -> run();
